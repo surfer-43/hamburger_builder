cb4588418c9bd14ff372a399e2756cbbf9cf29ff
Joel Brown <surfer_43@hotmail.com>
2020-04-22 10:01:47 -0400

    fixing differences in nav Items

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -9,6 +9,7 @@ const navItems = () => {
         <ul className={classes.NavItems}>
             <NavItem 
                 link="/"
+                exact
             >Burger Builder</NavItem>
             <NavItem 
                 link="/orders"
